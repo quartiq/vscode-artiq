4f7a7b5229484d94d5053b9bbeadd7f7b28fd25e
Phillip Klein <pk@quartiq.de>
2026-02-05 12:36:50 +0100

    Add roundtrip test

--- src/pyon/pyon.test.ts
+++ src/pyon/pyon.test.ts
@@ -1,6 +1,8 @@
 import { describe, it, expect } from "vitest";
 import * as pyon from "./pyon.js";
 
+let normalize = (s: string): string => JSON.stringify(JSON.parse(s));
+
 let hinted = `
     {
         "__jsonclass__": [
@@ -24,6 +26,19 @@ describe("decode", () => {
 
 describe("encode", () => {
     it("should transform TypeTaggedObject into HintedJsonClass", () => {
-        expect(pyon.encode(tagged)).toBe(hinted.replaceAll(/\s/g,""));
+        expect(normalize(pyon.encode(tagged))).toBe(normalize(hinted));
+    });
+});
+
+import { join } from "path";
+import { readFileSync } from "fs";
+
+let p = join(__dirname, "pyon_v2.test.json");
+let _hinted = readFileSync(p, "utf8");
+let _tagged = pyon.decode(_hinted);
+
+describe("roundtrip", () => {
+    it("should be identical to the input JSON", () => {
+        expect(normalize(pyon.encode(_tagged))).toBe(normalize(_hinted));
     });
 });
---
+++ src/pyon/pyon_v2.test.py
@@ -0,0 +1,34 @@
+# Prints the test object from
+# sipyco/test/test_pyon.py
+
+from fractions import Fraction
+from collections import OrderedDict
+
+import numpy as np
+from sipyco import pyon
+
+print(pyon.encode({
+    None: False,
+    True: b"bytes",
+
+    # FIXME: skip float("inf"), because it gets serialized into invalid JSON
+    # same goes for float("-inf") and float("nan")
+    # see: https://docs.python.org/3/library/json.html#infinite-and-nan-number-values
+    #
+    #float("inf"): float("-inf"),
+
+    (1, 2): [(3, 4.2), (2, )],
+    "slice": slice(3),
+    Fraction(3, 4): np.linspace(5, 10, 1),
+    "set": {"testing", "sets"},
+    "a": np.int8(9), "b": np.int16(-98), "c": np.int32(42), "d": np.int64(-5),
+    "e": np.uint8(8), "f": np.uint16(5), "g": np.uint32(4), "h": np.uint64(9),
+    "x": np.float16(9.0), "y": np.float32(9.0), "z": np.float64(9.0),
+    1j: 1-9j,
+    "q": np.complex128(1j),
+    "zerodim": np.array(0),
+    "od": OrderedDict(zip(reversed(range(3)), "abc")),
+    "unicode": "\u269B",
+    "newline": "\n" """
+""",
+}, True))
