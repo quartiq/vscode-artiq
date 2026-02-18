78477c011593cfebc09c42f18586b89fa2bd4efb
Phillip Klein <pk@quartiq.de>
2026-02-18 12:42:18 +0100

    Express bytes pyon type through Uint8Array instead of Array

--- src/pyon/bytes.ts
+++ src/pyon/bytes.ts
@@ -1,25 +1,19 @@
-type Bytes = number[];
+import * as utils from "../utils.js";
+
+type Bytes = Uint8Array;
 type Params = [ base64: string ];
 
-// FIXME: use Uint8Array.fromBase64() as soon it is available
-// see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/fromBase64
-export let fromMachine = (params: any[]): Bytes => {
-    let chars = [ ...atob((params as Params)[0]) ];
-    return chars.map(c => c.charCodeAt(0)) as Bytes;
-};
+export let fromMachine = ([ base64 ]: any[]): Bytes =>
+    utils.bytesFrom(base64) as Bytes;
 
-// FIXME: use Uint8Array.prototype.toBase64() as soon it is available
-// see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/toBase64
-export let toMachine = (data: any): Params => {
-    let base64 = btoa(String.fromCharCode(...(data as Bytes)));
-    return [ base64 ] as Params;
-};
+export let toMachine = (data: any): Params =>
+    [ utils.base64From(data as Bytes) ] as Params;
 
 export let fromHuman = fromMachine;
 export let toHuman = toMachine;
 
 // FIXME: use Uint8Array.prototype.toHex() as soon it is available
 // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/toHex
-export let forPreview = (data: any): string[] => (data as Bytes).map(b => b.toString(16));
+export let forPreview = (data: any): string[] => Array.from(data as Bytes).map(b => b.toString(16));
 
 export let copy = (src: any): any => [ ...src ];
